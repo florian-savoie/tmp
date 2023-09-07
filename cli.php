4caecc232660ba72c2901dac70fdcbb8169d7617
florian-savoie <florianpsp303@gmail.com>
2023-09-07 23:29:01 +0200

    maj

---
+++ cli.php
@@ -0,0 +1,34 @@
+<?php
+  require('head.php');
+  ?>
+<!-- Layout wrapper -->
+  <div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+ 
+
+      <!-- Layout container -->
+      <div class="layout-page">
+
+        <!-- Content wrapper -->
+        <div class="content-wrapper">
+          <!-- Content -->
+
+          <div class="container-xxl flex-grow-1 container-p-y">
+
+          </div>
+          <!-- / Content -->
+
+
+        </div>
+        <!-- Content wrapper -->
+      </div>
+      <!-- / Layout page -->
+    </div>
+
+
+
+  </div>
+  <!-- / Layout wrapper -->
+  <?php
+  require('footer.php');
+  ?>
