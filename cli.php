ca73d17453e8a2ba0b81cbe444eecbb3c4f21401
florian-savoie <florianpsp303@gmail.com>
2023-09-08 00:04:08 +0200

    maj

--- cli.php
+++ cli.php
@@ -1,34 +1,209 @@
 <?php
   require('head.php');
   ?>
-<!-- Layout wrapper -->
-  <div class="layout-wrapper layout-content-navbar">
-    <div class="layout-container">
+
  
 
-      <!-- Layout container -->
-      <div class="layout-page">
-
-        <!-- Content wrapper -->
-        <div class="content-wrapper">
-          <!-- Content -->
-
-          <div class="container-xxl flex-grow-1 container-p-y">
-
-          </div>
-          <!-- / Content -->
-
-
-        </div>
-        <!-- Content wrapper -->
-      </div>
-      <!-- / Layout page -->
-    </div>
 
 
 
-  </div>
-  <!-- / Layout wrapper -->
+            <div class="container-fluid flex-grow-1 container-p-y" style="width: 100%;">
+
+              <div class="row">
+                <div class="col-6" >
+                  <div class="card mb-4"  style="height: 100%;">
+                    <div class="user-profile-header-banner">
+                      <img src="./assets/img/pages/profile-banner.png" alt="Banner image" class="rounded-top img-fluid" />
+                    </div>
+                    <div class="user-profile-header d-flex flex-column flex-sm-row text-sm-start text-center mb-4">
+                      <div class="flex-shrink-0 mt-n2 mx-sm-0 mx-auto">
+                        <img
+                          src="./assets/img/avatars/1.png"
+                          alt="user image"
+                          class="d-block h-auto ms-0 ms-sm-4 rounded user-profile-img" />
+                      </div>
+                      <div class="flex-grow-1 mt-3 mt-sm-5">
+                        <div
+                          class="d-flex align-items-md-end align-items-sm-start align-items-center justify-content-md-between justify-content-start mx-4 flex-md-row flex-column gap-4">
+                          <div class="user-profile-info">
+                            <h4>John Doe</h4>
+                            <ul
+                              class="list-inline mb-0 d-flex align-items-center flex-wrap justify-content-sm-start justify-content-center gap-2">
+                              <li class="list-inline-item">
+                                <i class="mdi mdi-invert-colors me-1 mdi-20px"></i
+                                ><span class="fw-semibold">UX Designer</span>
+                              </li>
+                              <li class="list-inline-item">
+                                <i class="mdi mdi-map-marker-outline me-1 mdi-20px"></i
+                                ><span class="fw-semibold">Vatican City</span>
+                              </li>
+                              <li class="list-inline-item">
+                                <i class="mdi mdi-calendar-blank-outline me-1 mdi-20px"></i
+                                ><span class="fw-semibold"> Joined April 2021</span>
+                              </li>
+                            </ul>
+                          </div>
+                          <a href="javascript:void(0)" class="btn btn-primary">
+                            <i class="mdi mdi-account-check-outline me-1"></i>Connected
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-6">
+                    <div class="card" style="height:100%;">
+                     
+                      <div class="card-body pt-0 pb-3">
+                        <div class="row d-flex align-items-center">
+                            <div class="col-6">
+                                <div class="card-info">
+                                  <div class="d-flex align-items-center gap-2 flex-wrap">
+                                    <h5 class="mb-0">22k</h5>
+                                    <div class="d-flex text-success">
+                                      <p class="mb-0">+15%</p>
+                                      <div class="mdi mdi-chevron-up"></div>
+                                    </div>
+                                  </div>
+                                  <p class="mb-0 mt-1">Total Order</p>
+                                </div>
+                              </div>
+                          <div class="col-6">
+                            <div class="card-info">
+                              <div class="d-flex align-items-center gap-2 flex-wrap">
+                                <h5 class="mb-0">22k</h5>
+                                <div class="d-flex text-success">
+                                  <p class="mb-0">+15%</p>
+                                  <div class="mdi mdi-chevron-up"></div>
+                                </div>
+                              </div>
+                              <p class="mb-0 mt-1">Total Order</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <hr class="my-2">
+                      <div class="card-body pt-0 pb-3">
+                        <div class="row d-flex align-items-center">
+                            <div class="col-6">
+                                <div class="card-info">
+                                  <div class="d-flex align-items-center gap-2 flex-wrap">
+                                    <h5 class="mb-0">22k</h5>
+                                    <div class="d-flex text-success">
+                                      <p class="mb-0">+15%</p>
+                                      <div class="mdi mdi-chevron-up"></div>
+                                    </div>
+                                  </div>
+                                  <p class="mb-0 mt-1">Total Order</p>
+                                </div>
+                              </div>
+                          <div class="col-6">
+                            <div class="card-info">
+                              <div class="d-flex align-items-center gap-2 flex-wrap">
+                                <h5 class="mb-0">22k</h5>
+                                <div class="d-flex text-success">
+                                  <p class="mb-0">+15%</p>
+                                  <div class="mdi mdi-chevron-up"></div>
+                                </div>
+                              </div>
+                              <p class="mb-0 mt-1">Total Order</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <hr class="my-2">
+                      <div class="card-body pt-0 pb-3">
+                        <div class="row d-flex align-items-center">
+                            <div class="col-6">
+                                <div class="card-info">
+                                  <div class="d-flex align-items-center gap-2 flex-wrap">
+                                    <h5 class="mb-0">22k</h5>
+                                    <div class="d-flex text-success">
+                                      <p class="mb-0">+15%</p>
+                                      <div class="mdi mdi-chevron-up"></div>
+                                    </div>
+                                  </div>
+                                  <p class="mb-0 mt-1">Total Order</p>
+                                </div>
+                              </div>
+                          <div class="col-6">
+                            <div class="card-info">
+                              <div class="d-flex align-items-center gap-2 flex-wrap">
+                                <h5 class="mb-0">22k</h5>
+                                <div class="d-flex text-success">
+                                  <p class="mb-0">+15%</p>
+                                  <div class="mdi mdi-chevron-up"></div>
+                                </div>
+                              </div>
+                              <p class="mb-0 mt-1">Total Order</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <hr class="my-2">
+                      <div class="card-body pt-0 pb-3">
+                        <div class="row d-flex align-items-center">
+                            <div class="col-6">
+                                <div class="card-info">
+                                  <div class="d-flex align-items-center gap-2 flex-wrap">
+                                    <h5 class="mb-0">22k</h5>
+                                    <div class="d-flex text-success">
+                                      <p class="mb-0">+15%</p>
+                                      <div class="mdi mdi-chevron-up"></div>
+                                    </div>
+                                  </div>
+                                  <p class="mb-0 mt-1">Total Order</p>
+                                </div>
+                              </div>
+                          <div class="col-6">
+                            <div class="card-info">
+                              <div class="d-flex align-items-center gap-2 flex-wrap">
+                                <h5 class="mb-0">22k</h5>
+                                <div class="d-flex text-success">
+                                  <p class="mb-0">+15%</p>
+                                  <div class="mdi mdi-chevron-up"></div>
+                                </div>
+                              </div>
+                              <p class="mb-0 mt-1">Total Order</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <hr class="my-2">
+                      <div class="card-body pt-0 pb-3">
+                        <div class="row d-flex align-items-center">
+                            <div class="col-6">
+                                <div class="card-info">
+                                  <div class="d-flex align-items-center gap-2 flex-wrap">
+                                    <h5 class="mb-0">22k</h5>
+                                    <div class="d-flex text-success">
+                                      <p class="mb-0">+15%</p>
+                                      <div class="mdi mdi-chevron-up"></div>
+                                    </div>
+                                  </div>
+                                  <p class="mb-0 mt-1">Total Order</p>
+                                </div>
+                              </div>
+                          <div class="col-6">
+                            <div class="card-info">
+                              <div class="d-flex align-items-center gap-2 flex-wrap">
+                                <h5 class="mb-0">22k</h5>
+                                <div class="d-flex text-success">
+                                  <p class="mb-0">+15%</p>
+                                  <div class="mdi mdi-chevron-up"></div>
+                                </div>
+                              </div>
+                              <p class="mb-0 mt-1">Total Order</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+
+            </div>
+
   <?php
   require('footer.php');
   ?>
